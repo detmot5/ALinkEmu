e00caa6fc568ff248049789f783e7b9f1dc53f82
Norbert Bielak <norbertbielak@gmail.com>
2021-11-17 21:27:47 +0100

    Added RegisterBitLocation and moved file

---
+++ ALinkEmu/Src/EmulationPlatform/AVR/Utils.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Norbert Bielak on 16.11.2021.
+//
+#pragma once
+
+#include <stdint.h>
+
+namespace ALinkEmu::AVR {
+
+using RamAddress = uint16_t;
+using FlashAddress = uint32_t;
+
+struct RegisterBitLocation {
+  RamAddress address;
+  uint8_t offset
+};
+
+}
--- ALinkEmu/Src/EmulationPlatform/Utils.hpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Norbert Bielak on 16.11.2021.
-//
-#pragma once
-
-#include <stdint.h>
-
-using AvrRamAddress = uint16_t;
-using AvrFlashAddress = uint32_t;
