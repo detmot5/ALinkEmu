a76cb3ed7d2edc0434c074bcc019d5fe935dcd27
Norbert Bielak <norbertbielak@gmail.com>
2021-11-18 22:13:55 +0100

    added single instuction execution

--- Sandbox/Main.cpp
+++ Sandbox/Main.cpp
@@ -10,9 +10,14 @@
 int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
   Logger::Init();
-  ALinkEmu::ATMega328p chip;
-  chip.Start();
-  chip.Run();
-  std::cout << "Start..." << std::endl;
+  ALinkEmu::AVR::Core avrCore;
+  avrCore.Init();
+  uint8_t data[2] = {0x0c, 0x01};
+  avrCore.LoadFirmware(data, 2);
+  avrCore.ExecuteSingleInstruction();
+
+
+
+
   return app.exec();
 }
