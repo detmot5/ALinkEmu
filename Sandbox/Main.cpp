3dc17a9bf5e41c8578cc6f511829fa799ceb6530
Norbert Bielak <norbertbielak@gmail.com>
2021-11-24 21:02:52 +0100

    added executing add cpc and sbc instructions

--- Sandbox/Main.cpp
+++ Sandbox/Main.cpp
@@ -12,12 +12,24 @@ int main(int argc, char* argv[]) {
   Logger::Init();
   ALinkEmu::AVR::Core avrCore;
   avrCore.Init();
-  uint8_t data[2] = {0x0c, 0x01};
-  avrCore.LoadFirmware(data, 2);
+
+  // These variables cast to uint8_t* are the same values
+  // Since AVR, x86 and any modern CPU are little endian the byte order of multibyte number is reversed
+  // So then, array {0x01, 0x0c} in memory looks: 0x0c01
+  uint16_t data = 0x0c01;
+  uint8_t dataBytes[6] = {
+      0x01, 0x0c,  // add r0, r1
+      0x01, 0x04,  // cpc r0, r1
+      0x10, 0x08   // sbc r1, r0
+  };
+
+  avrCore.LoadFirmware(dataBytes, 6);
   avrCore.ExecuteSingleInstruction();
-
-
-
+  avrCore.DumpCoreData();
+  avrCore.ExecuteSingleInstruction();
+  avrCore.DumpCoreData();
+  avrCore.ExecuteSingleInstruction();
+  avrCore.DumpCoreData();
 
   return app.exec();
 }
