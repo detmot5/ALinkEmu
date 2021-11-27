2f2ea287ac4f7ba217e30d549159df448e948ac1
Norbert Bielak <norbertbielak@gmail.com>
2021-11-27 12:59:09 +0100

    fixed assertions

--- ALinkEmu/Src/Base/Base.hpp
+++ ALinkEmu/Src/Base/Base.hpp
@@ -3,15 +3,16 @@
 //
 #pragma once
 
-#include "Logger.hpp"
-
 #include <csignal>
+
+#include "Logger.hpp"
 #define EMU_DEBUG_BREAK() raise(SIGABRT)
 
+#define EMU_ASSERT(x, ...)                                       \
+  if (!(x)) {                                                    \
+    EMU_LOG_CRITICAL("ALinkEmu debug assertion failed!"); \
+    EMU_LOG_CRITICAL(__VA_ARGS__);                        \
+    EMU_DEBUG_BREAK();                                            \
+  }
 
-#define EMU_ASSERT(x, ...) if(!(x)) EMU_CLIENT_LOG_CRITICAL("ALinkEmu debug assertion failed!"); \
-                                        EMU_CLIENT_LOG_CRITICAL(__VA_ARGS__); EMU_DEBUG_BREAK()
-
-
-namespace ALinkEmu {
-}
+namespace ALinkEmu {}
