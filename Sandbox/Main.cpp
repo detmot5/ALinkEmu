9bdd0aff0ad430a523b2a5f151c4bc8e1c3ab859
Norbert Bielak <norbertbielak@gmail.com>
2021-11-27 13:00:36 +0100

    clenaup

--- Sandbox/Main.cpp
+++ Sandbox/Main.cpp
@@ -18,8 +18,8 @@ int main(int argc, char* argv[]) {
   // So then, array {0x01, 0x0c} in memory looks: 0x0c01
   uint16_t data = 0x0c01;
   uint8_t dataBytes[6] = {
-      0x01, 0x0c,  // add r0, r1
       0x01, 0x04,  // cpc r0, r1
+      0x01, 0x0c,  // add r0, r1
       0x10, 0x08   // sbc r1, r0
   };
 
