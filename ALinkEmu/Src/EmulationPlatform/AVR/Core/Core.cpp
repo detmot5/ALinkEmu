6b728ccd48dbe5de75be58d927f9791186a46135
Norbert Bielak <norbertbielak@gmail.com>
2021-11-27 12:59:27 +0100

    added movw and fixed memory problems

--- ALinkEmu/Src/EmulationPlatform/AVR/Core/Core.cpp
+++ ALinkEmu/Src/EmulationPlatform/AVR/Core/Core.cpp
@@ -5,6 +5,7 @@
 #include "Core.hpp"
 
 #include <cstring>
+#include <sstream>
 
 #include "Base/Base.hpp"
 #include "EmulationPlatform/AVR/InstructionExecutor/InstructionExecutor.hpp"
@@ -12,18 +13,22 @@
 namespace ALinkEmu::AVR {
 
 void Core::Init() {
+  // 328p memory stats;
+  this->platformDependentData.FLASHEND = 32768;
+  this->platformDependentData.RAMEND = 2048;
+
   this->state = CpuState::BEFORE_INIT;
-  this->flash = std::make_shared<uint16_t[]>(this->platformDependentData.FLASHEND + 4);
+  this->flash = std::unique_ptr<uint8_t[]>(new uint8_t[this->platformDependentData.FLASHEND + 4]);
   std::memset(this->flash.get(), 0xFF, this->platformDependentData.FLASHEND + 1);
   this->codeEnd = this->platformDependentData.FLASHEND;
-  this->ram = std::make_shared<uint8_t[]>(this->platformDependentData.RAMEND + 1);
+  this->ram = std::unique_ptr<uint8_t[]>(new uint8_t[this->platformDependentData.RAMEND + 1]);
   std::memset(this->ram.get(), 0x00, this->platformDependentData.RAMEND);
   this->frequency = 10000000;
   this->instructionExecutor.AttachCore(this);
   this->PC = 0;
 
   this->ram[0] = 10;
-  this->ram[1] = 250;
+  this->ram[1] = 10;
 }
 
 void Core::Reset() {}
@@ -32,8 +37,6 @@ void Core::ExecuteSingleInstruction() {
   uint32_t opcode = this->FetchInstruction(this->PC);
   uint32_t newPC = this->PC + 1;
 
-  EMU_LOG_WARN("{0}", opcode & 0xFC00);
-
   switch (opcode & 0xF000) {
     case 0x0000: {
       switch (opcode) {
@@ -41,7 +44,7 @@ void Core::ExecuteSingleInstruction() {
           EMU_LOG_WARN("NOP");
         } break;
         default: {
-          switch (opcode & 0xFF00) {
+          switch (opcode & 0xFC00) {
             case 0x0400: {
               this->instructionExecutor.CPC(opcode);
             } break;
@@ -51,6 +54,12 @@ void Core::ExecuteSingleInstruction() {
             case 0x0800: {
               this->instructionExecutor.SBC(opcode);
             } break;
+            default:
+              switch (opcode & 0xFF00) {
+                case 0x0100: {
+                  this->instructionExecutor.MOVW(opcode);
+                }
+              }
           }
         }
       }
@@ -62,19 +71,27 @@ void Core::ExecuteSingleInstruction() {
   EMU_LOG_INFO("Instruction executed. Core dump: \n{0}", this->DumpCoreData());
 }
 
-uint32_t Core::FetchInstruction(FlashAddress currentPC) { return this->flash[PC]; }
+uint32_t Core::FetchInstruction(FlashAddress currentPC) {
+  return this->flash[currentPC] | (this->flash[currentPC + 1] << 8);
+}
 
 void Core::LoadFirmware(uint8_t* data, size_t size) { std::memcpy(this->flash.get(), data, size); }
 
 // Simple way to serialize core registers values (especially SREG) for debugging purposes
 std::string Core::DumpCoreData() {
-  std::string res =
-      fmt::format("SREG: I = {0} T = {1}  H = {2} S = {3} V = {4} N = {5} Z = {6} C = {7}",
-                  this->GetSregFlagValue(SregFlag::I), this->GetSregFlagValue(SregFlag::T),
-                  this->GetSregFlagValue(SregFlag::H), this->GetSregFlagValue(SregFlag::S),
-                  this->GetSregFlagValue(SregFlag::V), this->GetSregFlagValue(SregFlag::N),
-                  this->GetSregFlagValue(SregFlag::Z), this->GetSregFlagValue(SregFlag::C));
-  return res;
+  std::string sreg = fmt::format("SREG: I = {0} T = {1}  H = {2} S = {3} V = {4} N = {5} Z = {6} C = {7}\n",
+                                 this->GetSregFlagValue(SregFlag::I), this->GetSregFlagValue(SregFlag::T),
+                                 this->GetSregFlagValue(SregFlag::H), this->GetSregFlagValue(SregFlag::S),
+                                 this->GetSregFlagValue(SregFlag::V), this->GetSregFlagValue(SregFlag::N),
+                                 this->GetSregFlagValue(SregFlag::Z), this->GetSregFlagValue(SregFlag::C));
+
+  std::string generalPurposeRegisters;
+  for (uint8_t i = 0; i < 32; i++) {
+    uint8_t reg = this->GetRegisterValue(i);
+    generalPurposeRegisters += fmt::format("r{0}={1} ", i, reg);
+  }
+
+  return sreg + generalPurposeRegisters;
 }
 
 }  // namespace ALinkEmu::AVR
--- ALinkEmu/Src/EmulationPlatform/AVR/Core/Core.hpp
+++ ALinkEmu/Src/EmulationPlatform/AVR/Core/Core.hpp
@@ -17,6 +17,25 @@ enum class CpuState {
   CRASHED,
 };
 
+struct PlatformDependentData {
+  // Last address of IO Section
+  uint16_t IOEND;
+  // Last address of RAM
+  uint16_t RAMEND;
+  // Last address of Flash memory
+  uint32_t FLASHEND;
+  // Last Address of EEPROM memory
+  uint32_t E2END;
+  uint16_t RAMPZ;  // optional, only for ELPM/SPM on >64Kb cores
+  uint16_t EIND;   // optional, only for EIJMP/RECALL on >64Kb cores
+  uint8_t vectorSize;
+  uint8_t signature[3];
+  uint8_t fuse[6];
+  uint8_t lockbits;
+  uint8_t addressSize;  // 2, or 3 for cores > 128KB in flash
+};
+
+
 class Core {
   friend class InstructionExecutor;
  public:
@@ -46,28 +65,12 @@ class Core {
   CpuState state;
   // Program Counter
   FlashAddress PC;
-  std::shared_ptr<uint8_t[]> ram;
-  std::shared_ptr<uint16_t[]> flash;
+  std::unique_ptr<uint8_t[]> ram;
+  std::unique_ptr<uint8_t[]> flash;
   uint32_t codeEnd;
   uint32_t frequency;
 
-  struct PlatformDependentData {
-    // Last address of IO Section
-    uint16_t IOEND;
-    // Last address of RAM
-    uint16_t RAMEND;
-    // Last address of Flash memory
-    uint32_t FLASHEND;
-    // Last Address of EEPROM memory
-    uint32_t E2END;
-    uint16_t RAMPZ;  // optional, only for ELPM/SPM on >64Kb cores
-    uint16_t EIND;   // optional, only for EIJMP/RECALL on >64Kb cores
-    uint8_t vectorSize;
-    uint8_t signature[3];
-    uint8_t fuse[6];
-    uint8_t lockbits;
-    uint8_t addressSize;  // 2, or 3 for cores > 128KB in flash
-  } platformDependentData;
+  PlatformDependentData platformDependentData;
 
   struct MicrocontrollerStatusRegister {
     // Power On Reset Flag
@@ -82,5 +85,7 @@ class Core {
 
   bool sregMirror[8] = {0};
 };
+
+
 
 }  // namespace ALinkEmu::AVR
