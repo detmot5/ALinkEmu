874d05d466989d37d8967080d3ab1bec53779831
Norbert Bielak <norbertbielak@gmail.com>
2021-12-01 22:47:31 +0100

    added tests for MULS #6

--- ALinkEmu/Test/AVRTests/InstructionsTest.cpp
+++ ALinkEmu/Test/AVRTests/InstructionsTest.cpp
@@ -3,14 +3,13 @@
 //
 #include <gtest/gtest.h>
 
-#include "../../ALinkEmu.hpp"
-
-ALinkEmu::AVR::Core avrCore;
 #include <array>
 
+#include "../../ALinkEmu.hpp"
 
 TEST(AVRTest, AddFunctionWithOverflowSregTests) {
   using namespace ALinkEmu::AVR;
+  ALinkEmu::AVR::Core avrCore;
   Logger::Init();
   avrCore.Init();
   uint8_t addOpcode[2] = {
@@ -32,4 +31,27 @@ TEST(AVRTest, AddFunctionWithOverflowSregTests) {
   EXPECT_EQ(avrCore.GetSregFlagValue(SregFlag::N), false);
   EXPECT_EQ(avrCore.GetSregFlagValue(SregFlag::Z), false);
   EXPECT_EQ(avrCore.GetSregFlagValue(SregFlag::C), true);
+}
+
+TEST(AVRInstructionTest, MULS) {
+  using namespace ALinkEmu::AVR;
+
+  ALinkEmu::AVR::Core avrCore;
+  avrCore.Init();
+  uint8_t mulsOpcode[2] = {
+      0x45, 0x02  // muls r20, r21
+  };
+
+  avrCore.SetRegisterValue(20, 30);
+  avrCore.SetRegisterValue(21, 30);
+  avrCore.LoadFirmware(mulsOpcode, 2);
+  avrCore.ExecuteSingleInstruction();
+
+  uint8_t R0result = avrCore.GetRegisterValue(0);
+  uint8_t R1result = avrCore.GetRegisterValue(1);
+
+  EXPECT_EQ(R0result, 0x84);
+  EXPECT_EQ(R1result, 0x03);
+  EXPECT_EQ(avrCore.GetSregFlagValue(SregFlag::C), (R1result >> 7) & 0x01);
+  EXPECT_EQ(avrCore.GetSregFlagValue(SregFlag::Z), 0);
 }
