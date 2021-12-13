88cbc055f449f51463bcb0bc2fb19d2878c7928a
Norbert Bielak <norbertbielak@gmail.com>
2021-12-13 22:14:13 +0100

    Added macro for binding member fns for callbacks

--- ALinkEmu/Src/Base/Base.hpp
+++ ALinkEmu/Src/Base/Base.hpp
@@ -8,11 +8,14 @@
 #include "Logger.hpp"
 #define EMU_DEBUG_BREAK() raise(SIGABRT)
 
-#define EMU_ASSERT(x, ...)                                       \
-  if (!(x)) {                                                    \
+#define BIND_MEMBER_FUNCTION_TO_CALLBACK(fn) \
+  [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+
+#define EMU_ASSERT(x, ...)                                \
+  if (!(x)) {                                             \
     EMU_LOG_CRITICAL("ALinkEmu debug assertion failed!"); \
     EMU_LOG_CRITICAL(__VA_ARGS__);                        \
-    EMU_DEBUG_BREAK();                                            \
+    EMU_DEBUG_BREAK();                                    \
   }
 
 namespace ALinkEmu {}
